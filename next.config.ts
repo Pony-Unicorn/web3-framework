b43fcb76cb6e335ce8813797759f146dbfc03822
pony <13500815917@163.com>
2025-10-14 17:57:06 +0800

    add images unoptimized

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  // Docs: https://nextjs.org/docs/app/guides/static-exports
   output: 'export',
   images: {
     unoptimized: true,
