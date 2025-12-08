8fbc239103e02173b4fd2df6b2d94da1ad149c36
pony <13500815917@163.com>
2025-12-09 01:19:54 +0800

    add home ssg

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,6 @@ import type { Config } from '@react-router/dev/config'
 export default {
   ssr: false,
   async prerender() {
-    return ['/about']
+    return ['/', '/about']
   },
 } satisfies Config
